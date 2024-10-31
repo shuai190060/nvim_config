cf2f48a577540d8ecc688bba72b7fe85a50d2e5b
Shuhai Liao <shuai190060roy@gmail.com>
2024-10-31 08:56:36 +0100

    #3: added the gotmpl

--- lua/syntax-highlight.lua
+++ lua/syntax-highlight.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "go", "lua", "rust", "hcl" },
+  ensure_installed = { "go", "lua", "rust", "hcl" ,"gotmpl"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
